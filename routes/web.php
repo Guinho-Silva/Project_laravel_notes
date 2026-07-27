45bb51d7f77e0853e1ff5120ff92904998aede16
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-07-27 15:53:41 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    echo 'Hello World!';
+});
+
+
+Route::get('/about', function(){
+    echo 'About us';
 });
