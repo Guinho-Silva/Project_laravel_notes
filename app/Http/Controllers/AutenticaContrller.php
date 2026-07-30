6184985b0b734a1429fae6be1364a71f1f87b7b4
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-07-30 14:57:23 -0300

    Introducao a validação de dados

--- app/Http/Controllers/AutenticaContrller.php
+++ app/Http/Controllers/AutenticaContrller.php
@@ -13,9 +13,28 @@ class AutenticaContrller extends Controller
 
     // Pega todas as informações subimetidas pelo formulario
     public function loginSubmit(Request $request){
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+
+        // validação do formulario pelo lado do servidor
+        $request->validate(
+            [
+                // Verifica se há uma informação no campo
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // get user input
+
+        $username = $request->input('txt_username');
+
+        $password = $request->input('txt_password');
+        
+        echo 'OK';
+
+        // teste dos dados que estão sendo passado por impossivel
+        // echo $request->input('text_username');
+        // echo '<br>';
+        // echo $request->input('text_password');
     }
 
     // Método para carregar a view de login
