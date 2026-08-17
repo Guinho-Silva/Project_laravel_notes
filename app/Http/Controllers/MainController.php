c326f043c8cbaa86d34fb1b891f4b7e7f523747b
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-08-17 17:47:34 -0300

    Apresentando as notas do user logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,21 +15,13 @@ class MainController extends Controller
         // Pega o usuario logado na sessaõ
         $id= session('user.id');
 
-        // Pega o id retornando um array com suas informações
-        $user = User::find($id)->toArray();
 
         // pega as notas dele com base no id e retonar um array das notas
         $notes = User::find($id)->notes()->get()->toArray();
 
-        echo'<pre>';
-        print_r($user);
-        print_r($notes);
-
-        die();
-
         // mostra a view home
 
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote(){
